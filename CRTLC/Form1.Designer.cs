cb7a706bc3d197ec2c2a9cfc6a462917dd890f3f
Jules <appougly@gmail.com>
2020-10-18 15:56:17 +0200

    Ajoutez des fichiers projet.

---
+++ CRTLC/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CRTLC
+{
+    partial class WindowI
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowI));
+            this.dataLog = new System.Windows.Forms.ListView();
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // dataLog
+            // 
+            this.dataLog.BackColor = System.Drawing.Color.White;
+            this.dataLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Date,
+            this.value});
+            this.dataLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataLog.HideSelection = false;
+            this.dataLog.Location = new System.Drawing.Point(0, 0);
+            this.dataLog.Name = "dataLog";
+            this.dataLog.Size = new System.Drawing.Size(704, 441);
+            this.dataLog.TabIndex = 0;
+            this.dataLog.UseCompatibleStateImageBehavior = false;
+            this.dataLog.View = System.Windows.Forms.View.Details;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 200;
+            // 
+            // value
+            // 
+            this.value.Text = "Value";
+            this.value.Width = 400;
+            // 
+            // WindowI
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.dataLog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 2000);
+            this.MinimumSize = new System.Drawing.Size(720, 480);
+            this.Name = "WindowI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CTRL+L";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView dataLog;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader value;
+    }
+}
+
---
+++ CRTLC/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows;
+using System.IO;
+using Gma.System.MouseKeyHook;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRTLC
+{
+    public partial class WindowI : Form
+    {
+        public WindowI()
+        {
+            InitializeComponent();
+        }
+
+
+        Timer timer = new Timer();
+
+        string pathdir;
+
+        private void GetDirLog()
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    pathdir = fbd.SelectedPath;
+                    LoadData(fbd.SelectedPath);
+                    NewDay();
+                }
+            }
+        }
+
+        private void NewDay()
+        {
+            string date = "crtl@" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            if (!File.Exists(Path.Combine(pathdir, date)))
+            {
+                CreateFile(Path.Combine(pathdir, date));
+            }
+            Subscribe();
+        }
+
+        private IKeyboardMouseEvents mouse;
+        public void Subscribe()
+        {
+            mouse = Hook.GlobalEvents();
+            mouse.KeyPress += KeyPress;
+        }
+
+        private int back = 0;
+
+        private new void KeyPress(object sender, KeyPressEventArgs e) {
+            string date = "crtl@" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            if ((int)e.KeyChar == 22)
+            {
+                string text = Clipboard.GetText();
+                text = text.Replace("\n", "").Replace("\r", "");
+                string content = "" + DateTime.Now.ToString("HH-mm-ss") + " "+ text +"\n";
+                AddToFile(Path.Combine(pathdir, date), content);
+                dataLog.Items.Clear();
+                LoadData(pathdir);
+            }
+            else if((int)e.KeyChar == 12)
+            {
+                if(back>= memory.Count - 1)
+                {
+                    back = 0;
+                    if (memory[back] != null && memory[back] != String.Empty)
+                    {
+                        Clipboard.SetText(memory[back]);
+                    }
+                }
+                else
+                {
+                    if (memory[back] != null && memory[back] != String.Empty)
+                    {
+                        Clipboard.SetText(memory[back]);
+                    }
+                }
+                back++;
+            }
+        }
+   
+        private void CreateFile(string path)
+        {
+            File.AppendAllText(path,"");
+        }
+
+        private void AddToFile(string path,string content)
+        {
+            File.AppendAllText(path, content);
+        }
+
+        List<string> memory = new List<string>();
+
+        private void LoadData(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    if (Path.GetExtension(file) == ".log")
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        Array.Reverse(lines);
+                        CreateLine("[" + Path.GetFileName(file) + "]", "");
+                        foreach (string line in lines)
+                        {
+                            if (Regex.Match(line, @"(([0-9:]+)\s*((.?)*))") != null)
+                            {
+                                string date = line.Split(' ')[0];
+                                string content = line.Split(' ')[1];
+                                CreateLine(date, content);
+                                memory.Add(content);
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        private void CreateLine(string date,string content)
+        {
+            dataLog.Alignment = ListViewAlignment.Left;
+            if (date != String.Empty)
+            {
+                ListViewItem it = new ListViewItem();
+                ListViewItem.ListViewSubItem sub = new ListViewItem.ListViewSubItem();
+                it.Text = date;
+                sub.Text = content;
+                it.SubItems.Add(sub);
+                dataLog.Click += DataLog_Click;
+                dataLog.SelectedIndexChanged += DataLog_Click;
+                dataLog.Items.Add(it);
+            }
+        }
+
+        private void DataLog_Click(object sender, EventArgs e)
+        {
+            if(dataLog.SelectedItems.Count > 0)
+            {
+                ListViewItem it = dataLog.SelectedItems[0];
+                if (it.SubItems.Count >= 2)
+                {
+                    if (it.SubItems[1].Text != null && it.SubItems[1].Text != String.Empty)
+                    {
+                        Clipboard.SetText(it.SubItems[1].Text);
+                    }
+                }
+            }   
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            GetDirLog();
+        }
+
+    }
+}
